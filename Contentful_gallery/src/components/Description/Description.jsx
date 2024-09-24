bfe3bdadf4279b1d3ae8a845cf19898db96f51cb
maylova93 <142877128+maylova93@users.noreply.github.com>
2024-09-24 13:12:26 +0200

    Description

---
+++ Contentful_gallery/src/components/Description/Description.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { createClient } from 'contentful';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+export const Description = () => { // Export as named export
+  const [pageDescription, setPageDescription] = useState(null);
+
+  const client = createClient({
+    space: 'cj1111y5497q',
+    environment: 'master', // defaults to 'master' if not set
+    accessToken: 'TyhQAC7gfnpeaXbuel6GADvV5UE1F93M4sy91X2hKBY'
+  });
+
+  useEffect(() => {
+    client.getEntry('6Dj0ahErS0I8iTT5YVHRcW')
+      .then((entry) => {
+        if (entry.fields && entry.fields.pageDescription) {
+          setPageDescription(entry.fields.pageDescription);
+        } else {
+          console.error("Page description is missing.");
+        }
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <>
+      {pageDescription ? (
+        <div>
+          {documentToReactComponents(pageDescription)}
+        </div>
+      ) : (
+        <p>Loading page description...</p>
+      )}
+    </>
+  );
+};
